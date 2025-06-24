8d449930b6324b5269455c5a2333f1dad6604211
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-06-24 16:44:19 +0300

    feat services, add mobile version

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -4,25 +4,25 @@ import { SwimCard } from "@/common/components/swimCard/swimCard";
 export default function MyServices() {
   return (
     <div className={s.worksWrapper}>
-      <div>
+      <div className={s.yearsAndExpirience}>
         <div className={s.years}>3+</div>
         <div className={s.myExpirience}>года опыта преподавания</div>
       </div>
 
       <div>
         <div className={s.slogan}>
-          Научись плавать уверенно — в любом возрасте
+          Научись плавать уверенно в любом возрасте
         </div>
         <div className={s.typesOfServices}>
           <SwimCard
             image={`/assets/1from3.jpg`}
             alt="children swim"
-            caption="Плавание грудных детей и старше"
+            caption="Обучение плаванию взрослых с нуля"
           />
           <SwimCard
             image={`/assets/2from3.jpg`}
             alt="adult swim"
-            caption="Обучение плаванию взрослых с нуля"
+            caption="Плавание грудных детей и старше"
           />
           <SwimCard
             image={`/assets/3from3.jpg`}
